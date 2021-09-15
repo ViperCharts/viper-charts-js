5b84f5fd6fbad0c9c71e956f4eb8c3806efcf1d4
Tryston Perry <trystonmperry@gmail.com>
2021-09-15 09:38:02 -0700

    Don't render time and price selected if cursor is offscreen

--- src/components/canvas_components/price_selected.js
+++ src/components/canvas_components/price_selected.js
@@ -12,6 +12,8 @@ export default class TimeSelected extends Layer {
     const p = crosshairState.crosshair.price;
     const y = crosshairState.crosshair.y;
 
+    if (y < 0) return;
+
     this.canvas.drawBox("#424242", [0, y - 10, 50, 20]);
     this.canvas.drawText("#fff", [25, y + 3], p);
   }
