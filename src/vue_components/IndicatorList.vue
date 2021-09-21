b5ec7f8e48083381b527ff2d196aecf7163d39d0
Tryston Perry <trystonmperry@gmail.com>
2021-09-21 08:59:25 -0700

    WIP add indicator list

---
+++ src/vue_components/IndicatorList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="indicator">
+      <span class="indicator-title"> Footprint </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    indicators: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.indicator {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
