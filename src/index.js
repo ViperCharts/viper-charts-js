93c8704fdd27a73de6b3d54744d74584cfa810de
Tryston Perry <trystonmperry@gmail.com>
2021-12-19 10:09:12 -0800

    Added support for Kucoin, FTX, and Binance

--- src/index.js
+++ src/index.js
@@ -5,27 +5,17 @@ import Constants from "./constants";
 let chart;
 
 (async () => {
-  const pairs = await (
-    await fetch("https://api.exchange.coinbase.com/products")
-  ).json();
+  const res = await fetch("http://demo-api.vipercharts.com/sources");
+  if (!res.ok) {
+    alert("An error occurred when fetching available markets.");
+    return;
+  }
 
-  const COINBASE = pairs.map((pair) => ({
-    source: "COINBASE",
-    name: pair.id,
-    maxItemsPerRequest: 300,
-    timeframes: [
-      Constants.MINUTE,
-      Constants.MINUTE5,
-      Constants.MINUTE15,
-      Constants.HOUR,
-      Constants.HOUR * 6,
-      Constants.DAY,
-    ],
-  }));
+  const sources = await res.json();
 
   // Actual chart stuff
   chart = new Chart({
-    sources: { COINBASE },
+    sources,
     initialSettings: JSON.parse(localStorage.getItem("settings") || "{}"),
     onRequestHistoricalData,
     onSaveViperSettings,
@@ -33,34 +23,18 @@ let chart;
 
   async function onRequestHistoricalData({ requests, callback }) {
     for (let { id, source, name, timeframe, start, end } of requests) {
-      if (source === "COINBASE") {
-        start = new Date(start).toISOString();
-        end = new Date(end).toISOString();
+      const res = await fetch(
+        `http://demo-api.vipercharts.com/candles?source=${source}&name=${name}&timeframe=${timeframe}&start=${start}&end=${end}`
+      );
 
-        const seconds = timeframe / 1000;
-        const res = await fetch(
-          `https://api.exchange.coinbase.com/products/${name}/candles?granularity=${seconds}&start=${start}&end=${end}`
-        );
-
-        if (res.status !== 200) {
-          return;
-        }
-
-        const data = {};
-
-        const json = (await res.json()).reverse();
-        for (const item of json) {
-          data[item[0] * 1000] = {
-            low: item[1],
-            high: item[2],
-            open: item[3],
-            close: item[4],
-            volume: item[5],
-          };
-        }
-
-        callback(id, data);
+      if (!res.ok) {
+        callback(id, {});
+        return;
       }
+
+      const data = await res.json();
+
+      callback(id, data);
     }
   }
 
