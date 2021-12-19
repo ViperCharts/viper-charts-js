f38693c82b9e29c5f25014327e1de66965416471
Tryston Perry <trystonmperry@gmail.com>
2021-12-19 11:09:30 -0800

    Added SMA indicator

--- src/components/indicators.js
+++ src/components/indicators.js
@@ -13,18 +13,20 @@ import Footprint from "./canvas_components/primitives/footprint.js";
 
 export const series = [
   new Indicator("Candlestick", [], Candlestick),
-  new Indicator("Footprint", [], Footprint),
+  // new Indicator("Footprint", [], Footprint),
   new Indicator("Price Line", [], PriceLine),
 ];
 
 import VolumeBar from "./canvas_components/primitives/volume_bar.js";
 import VolumeBySideBar from "./canvas_components/primitives/volume_by_side_bar.js";
+import SMA from "./canvas_components/primitives/sma";
 import MASlope from "./canvas_components/primitives/ma_slope";
 
 export const indicators = [
   new Indicator("Volume", [], VolumeBar),
-  new Indicator("Volume By Side", [], VolumeBySideBar),
+  // new Indicator("Volume By Side", [], VolumeBySideBar),
   new Indicator("MA Slope", [], MASlope),
+  new Indicator("SMA", [], SMA),
 ];
 
 export const map = (() => {
