166d82075c327145e54ab39a187ddcb657eebed6
Tryston Perry <trystonmperry@gmail.com>
2022-02-04 10:20:52 -0800

    Updated dataset group design

--- src/react-components/chart/dataset-group/indicator/indicator.jsx
+++ src/react-components/chart/dataset-group/indicator/indicator.jsx
@@ -28,7 +28,6 @@ export default class Indicator extends React.Component {
   render() {
     const { indicator } = this.props;
     const v = indicator.visible;
-    const dataset = this.chart.datasets[indicator.datasetId];
 
     return (
       <div className={`indicator v-noselect ${v ? "" : "invisible"}`}>
