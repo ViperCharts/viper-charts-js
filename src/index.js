cfe43842a9d77a163aa3a670e2202bdea3a4e7a1
Tryston Perry <TrystonMPerry@gmail.com>
2022-08-02 16:53:10 +0000

    Cleanup (#12)
    * Viper demo now used Binance public API
    
    * Removed console.log
    
    * Cleaned up README

--- src/index.js
+++ src/index.js
@@ -1,164 +1,96 @@
 import "./style.css";
+
+// Import the library locally. In a production app, you would use:
+// import ViperCharts from "@viper-charts/viper-charts"
 import ViperCharts from "./viper";
-
-const apiURL =
-  process.env.NODE_ENV === "production"
-    ? "https://api.vipercharts.com"
-    : "http://localhost:3000";
-
-const wsURL =
-  process.env.NODE_ENV === "production"
-    ? "wss://api.vipercharts.com"
-    : "ws://localhost:3000";
 
 let Viper;
 
-// Firefox is broken
-if (typeof InstallTrigger !== "undefined") {
-  alert(
-    "Sorry, but FireFox is currently not supported. Please use Chrome, Brave, Opera, or any other chromium browser. A fix is planned before launch."
-  );
-}
+main();
+async function main() {
+  // The sources available to ViperCharts
+  const sources = {
+    BINANCE: await getSourcesFromBinance(),
+  };
 
-const subs = new Set();
+  async function getSourcesFromBinance() {
+    const res = await fetch("https://www.binance.com/api/v3/exchangeInfo");
+    const json = await res.json();
 
-const socket = new WebSocket(wsURL);
+    const sources = {};
 
-(async () => {
-  await new Promise((r) => {
-    socket.addEventListener("open", () => {
-      console.log("Connected to viper socket");
-      r();
+    for (const item of json.symbols) {
+      sources[item.symbol] = {
+        source: "BINANCE",
+        name: item.symbol,
+        maxItemsPerRequest: 500,
+        models: {
+          price: {
+            id: "price",
+            model: "ohlc",
+            name: "Price",
+            label: `Binance:${item.symbol}`,
+          },
+        },
+      };
+    }
 
-      // Ping the server every 15 seconds
-      setInterval(() => {
-        socket.send(JSON.stringify({ event: "ping" }));
-      }, 15e3);
-    });
-    socket.addEventListener("disconnect", () =>
-      console.log("Disconnected from Viper API")
-    );
-    socket.addEventListener("message", ({ data }) => {
-      data = JSON.parse(data);
-      if (data.event === "updates") {
-        for (const datasetId in data.data) {
-          const [source, name, timeframe, dataModel] = datasetId.split(":");
-
-          const d = {};
-          for (const time in data.data[datasetId]) {
-            d[new Date(+time).toISOString()] = data.data[datasetId][time];
-          }
-
-          Viper.addData({ source, name, timeframe, dataModel }, d);
-        }
-      }
-    });
-  });
-
-  const res = await fetch(`${apiURL}/api/markets/get`);
-  if (!res.ok) {
-    alert("An error occurred when fetching available markets.");
-    return;
+    return sources;
   }
 
-  const sources = await res.json();
-
-  // Actual chart stuff
   Viper = new ViperCharts({
     element: document.getElementById("chart"),
-    sources: sources.data,
+    sources,
     settings: JSON.parse(localStorage.getItem("settings")),
     onRequestHistoricalData,
-    onRemoveDatasetModel,
     onSaveViperSettings,
     onRequestTemplates,
   });
+}
 
-  async function onRequestHistoricalData({ requests }) {
-    const now = Date.now();
-    const timeseries = [];
-
-    for (let { source, name, timeframe, dataModels, start, end } of requests) {
+async function onRequestHistoricalData({ requests }) {
+  for (const { source, name, timeframe, dataModels, start, end } of requests) {
+    if (source === "BINANCE") {
       for (const dataModel of dataModels) {
-        // If start and end are the same, ignore
-        if (start === end) {
-          console.log(new Date(start), new Date(end));
-          continue;
+        const tf = {
+          60000: "1m",
+          [60000 * 5]: "5m",
+          [60000 * 15]: "15m",
+          [60000 * 60]: "1h",
+          [60000 * 60 * 4]: "4h",
+          [60000 * 60 * 24]: "1d",
+        }[timeframe];
+
+        const res = await fetch(
+          `https://www.binance.com/api/v3/klines?symbol=${name}&interval=${tf}&startTime=${start}&endTime=${end}`
+        );
+        const json = await res.json();
+
+        const data = {};
+
+        for (const item of json) {
+          const [timestamp, open, high, low, close] = item;
+
+          const isoString = new Date(timestamp).toISOString();
+
+          data[isoString] = {
+            open: +open,
+            high: +high,
+            low: +low,
+            close: +close,
+          };
         }
 
-        timeseries.push({
-          source,
-          name,
-          dataModel,
-          timeframe,
-          start,
-          end,
-        });
-
-        // If end time is greater than current time, subscribe to real time data
-        if (end >= now) {
-          const id = `${source}:${name}:${timeframe}:${dataModel}`;
-          if (subs.has(id)) continue;
-
-          socket.send(
-            JSON.stringify({
-              event: "subscribe",
-              data: { source, name, timeframe, dataModel },
-            })
-          );
-
-          subs.add(id);
-        }
+        Viper.addData({ source, name, timeframe, dataModel }, data);
       }
     }
-
-    for (let i = 0; i < timeseries.length; i += 25) {
-      (async () => {
-        const sources = timeseries.slice(i, i + 25);
-
-        const res = await fetch(`${apiURL}/api/timeseries/get`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            sources,
-          }),
-        });
-
-        const { data } = await res.json();
-
-        for (let { source, name, timeframe, dataModel } of sources) {
-          timeframe = +timeframe;
-          const resId = `${source}:${name}:${timeframe}:${dataModel}`;
-
-          const d = {};
-          if (data[resId]) {
-            for (const ts in data[resId].data) {
-              d[new Date(+ts).toISOString()] = data[resId].data[ts];
-            }
-          }
-
-          Viper.addData({ source, name, timeframe, dataModel }, d);
-        }
-      })();
-    }
   }
+}
 
-  function onRemoveDatasetModel({ source, name, timeframe, dataModel }) {
-    socket.send(
-      JSON.stringify({
-        event: "unsubscribe",
-        data: { source, name, timeframe, dataModel },
-      })
-    );
-    subs.delete(`${source}:${name}:${timeframe}:${dataModel}`);
-  }
+function onSaveViperSettings(settings) {
+  localStorage.setItem("settings", JSON.stringify(settings));
+}
 
-  function onSaveViperSettings(settings) {
-    localStorage.setItem("settings", JSON.stringify(settings));
-  }
-
-  async function onRequestTemplates() {
-    const res = await fetch(`${apiURL}/api/templates/get`);
-    return (await res.json()).data;
-  }
-})();
+function onRequestTemplates() {
+  return [];
+}
