309fc0fa83ea98cab9857f64b29169de907c6c6d
Tryston Perry <TrystonMPerry@gmail.com>
2022-08-09 21:24:53 +0000

    1.0.14 (#15)
    * Updated package version
    
    * Fixed build system

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,12 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const packageJson = require("./package.json");
 
 module.exports = merge(common, {
   mode: "production",
   entry: [path.join(__dirname, "src/viper.js")],
   output: {
-    filename: `viper.bundle.${packageJson.version}.js`,
+    filename: `viper.bundle.js`,
     library: "viper-charts",
     libraryTarget: "umd",
   },
